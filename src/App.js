720655f111ac94e8794a70e77ab9574d7b1bb688
zouyu2021 <98063638+zouyu2021@users.noreply.github.com>
2023-04-02 18:00:05 -0400

    - Added react-redux. - Finished login component ui

--- src/App.js
+++ src/App.js
@@ -4,23 +4,36 @@ import {Routes, Route} from "react-router";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import NotFound from "./components/NotFound";
+import {ChakraProvider} from "@chakra-ui/react";
+import {Provider} from "react-redux";
+import {configureStore} from "@reduxjs/toolkit";
+import userReducer from "./reducers/user-reducer";
+
+const store = configureStore({
+    reducer: {
+        user: userReducer,
+    }
+});
+
 const App = () => {
   return (
-      <>
-        <div>CS5610 Foodie</div>
-        <BrowserRouter>
-          <div className="container">
-            <Routes>
-                <Route path="/login"
-                          element={<Login />}/>
-                <Route path="/register"
-                            element={<Register />}/>
-                <Route path="/*"
-                            element={<NotFound />}/>
-            </Routes>
-            </div>
-        </BrowserRouter>
-      </>
+      <ChakraProvider>
+        <Provider store={store}>
+            <div>CS5610 Foodie</div>
+            <BrowserRouter>
+                <div className="container">
+                    <Routes>
+                        <Route path="/login"
+                               element={<Login />}/>
+                        <Route path="/register"
+                               element={<Register />}/>
+                        <Route path="/*"
+                               element={<NotFound />}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </Provider>
+      </ChakraProvider>
   )
 }
 
