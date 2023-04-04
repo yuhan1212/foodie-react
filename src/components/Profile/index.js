938bd701e5fee53a143e49ff35ebf162db64cd7a
stctai <84058750+stctai@users.noreply.github.com>
2023-04-03 23:41:10 -0700

    Made profile page more responsive

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,4 @@
 // profile screen
-// import {Container, Grid, GridItem, Image} from "@chakra-ui/react";
 import UserCard from "./user-card";
 import UserList from "./user-list";
 import SavedRecipe from "./saved-recipe";
@@ -8,18 +7,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Index = () => {
     // show all elements in the profile page just for debug purpose
     return (
-        <div className="container">
-            <img src="https://cdn.pixabay.com/photo/2022/04/30/19/12/cooking-banner-7166200__340.jpg" alt="Banner"/>
-            <h1 className="d-none d-sm-none d-md-none d-lg-block"
-                style={{marginTop: "-60px", marginLeft: "480px", position: "absolute"}}>
+        <div className="container border-start border-end">
+            <img src="https://cdn.pixabay.com/photo/2022/04/30/19/12/cooking-banner-7166200__340.jpg" alt="Banner" style={{width: "100%"}}/>
+            <h1 className="d-none d-sm-none d-md-block"
+                style={{marginTop: "-60px", marginLeft: "480px"}}>
                     User 123
             </h1>
-            <div style={{width: "500px", marginTop: "-15%", position: "absolute"}}>
-                <UserCard />
-            </div>
-            <div className="d-none d-sm-none d-md-none d-lg-block" style={{marginLeft: "525px"}}>
-                <UserList />
-                <SavedRecipe />
+            <div class="row">
+                <div className="col-3" style={{width: "500px", marginTop: "-15%"}}>
+                    <UserCard />
+                </div>
+                <div className="col m-3">
+                    <UserList />
+                    <SavedRecipe />
+                </div>
             </div>
         </div>
     )
