23085b6a0d8b71c5c96ed650fad162bf93d7d172
Kaylin Lau <lau.kay@northeastern.edu>
2023-04-04 16:56:28 -0700

    update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,46 +3,47 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-light">
-            <nav className="navbar bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
 
-                <div className="container-fluid">
-                    {/*logo*/}
-                    <a className="navbar-brand" href="#">
-                        <i className="bi bi-egg-fried"></i> Foodie</a>
+            <div className="container-fluid">
+               <NavLink to="/home" className="nav-link" data-activeclassname="active">
+                    <a className="navbar-brand" href="#"><i className="bi bi-egg-fried"></i> Foodie</a>
+               </NavLink>
 
-                    <div className="navbar-nav me-auto mb-2 mb-lg-0">
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <NavLink to="/home" className="nav-link" data-activeclassname="active">
                             Home
                         </NavLink>
-                    </div>
-                    <div className="navbar-nav me-auto mb-2 mb-lg-0">
                         <NavLink to="/Profile" className="nav-link" data-activeclassname="active">
                             Profile
                         </NavLink>
-                    </div>
-                    <div className="navbar-nav me-auto mb-2 mb-lg-0">
                         <NavLink to="/others" className="nav-link" data-activeclassname="active">
                             Others
                         </NavLink>
-                    </div>
-                    <div className="navbar-nav me-auto mb-2 mb-lg-0">
                         <NavLink to="/Aboutus" className="nav-link" data-activeclassname="active">
                             About us
                         </NavLink>
-                    </div>
-                    <div className="navbar-nav me-auto mb-2 mb-lg-0 float-end">
+
+                    </ul>
+                    <span className="navbar-text me-3">
                         <NavLink to="/login" className="nav-link" data-activeclassname="active">
                             Login
                         </NavLink>
-                    </div>
-                    <div className="navbar-nav me-auto mb-2 mb-lg-0 float-end">
+                    </span>
+                    <span className="navbar-text">
                         <NavLink to="/register" className="nav-link" data-activeclassname="active">
                             Register
                         </NavLink>
-                    </div>
+                    </span>
                 </div>
-            </nav>
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
+
         </nav>
     )
 }
