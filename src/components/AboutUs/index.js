9c37440e6f2fcfe16c29ec72eb2bb6852a046bd4
Kaylin Lau <lau.kay@northeastern.edu>
2023-04-05 11:19:25 -0700

    update about us

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -3,8 +3,21 @@ import React from 'react';
 const AboutUs = () => {
     return(
         <div>
-            About us ...
+            <h1>About Us</h1>
+           <p class="fs-4">
+               Welcome to [Website Name]! We are a team of food enthusiasts and tech-savvy individuals who are passionate about bringing delicious recipes and culinary inspiration to your fingertips.
+
+               At [Website Name], we believe that cooking and eating are not just about sustenance - they're about creativity, community, and connection. Whether you're cooking for one or feeding a crowd, we believe that everyone should have access to easy-to-follow recipes, fresh ingredients, and helpful cooking tips.
+           </p>
+            <figure className="text-center">
+                <blockquote className="blockquote">
+                    <p>Food is not just fuel, it's information. It talks to your DNA and tells it what to do.</p>
+                </blockquote>
+                <figcaption className="blockquote-footer">
+                    <cite title="Source Title">Dr. Mark Hyman</cite>
+                </figcaption>
+            </figure>
         </div>
     )
 }
-export default AboutUs;
+export default AboutUs
--- src/components/Others/index.js
+++ src/components/Others/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 const Others = () => {
     return(
-        <div>
-            you have to sign in first to join the foodies community
+        <div className='alert alert-light'>
+            Please log in first
         </div>
+
     )
 }
 export default Others;
