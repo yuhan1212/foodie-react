2aa5df44213a7781992bd5e3f9efacf4b45b1394
Kaylin Lau <lau.kay@northeastern.edu>
2023-04-04 18:56:08 -0700

    update home

--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -3,25 +3,19 @@ import React from 'react';
 const HomeScreen = () => {
 
     return (
-        <div>
-            <img src="../../../images/food.png" alt="display" />
-            <div className="container-fluid">
-
-                <div className="hero-section">
-                    <div className="hero-content">
-
-                        <h1>Delicious Recipes</h1>
-                        <p>Discover a world of mouth-watering dishes</p>
-
-                        <form className="form-inline">
-                            <div className="form-group">
-                                <input type="text" className="form-control" placeholder="Search for recipes"/>
-                            </div>
-                            <button type="submit" className="btn btn-primary">Search</button>
-                        </form>
+        <div className="position-relative">
+            <img src="../../../images/food.png" className="img-fluid w-100 h-100" alt="placeholder" />
+            <div className="position-absolute top-50 start-50 translate-middle">
+                <form className="form-inline my-2 my-lg-0">
+                    <div className="row">
+                        <div className="col">
+                            <input className="form-control mr-sm-2" style={{ width: '300px' }} type="search" placeholder="Search" aria-label="Search" />
+                        </div>
+                        <div className="col-auto">
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </div>
                     </div>
-                </div>
-
+                </form>
             </div>
         </div>
     )
