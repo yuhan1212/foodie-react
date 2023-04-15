3c58cd77693c9180f9e3c0c311d5a6938bf52743
Kaylin Lau <lau.kay@northeastern.edu>
2023-04-15 16:39:30 -0700

    edit

--- src/components/Others/index.js
+++ src/components/Others/index.js
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Others = () => {
-    const currentUser = useSelector(state => state.auth.currentUser);
+    // const currentUser = useSelector(state => state.auth.currentUser);
 
     return(
-
-        <div>
-            <h2>User Profile</h2>
-            <p>Name: {currentUser.name}</p>
-            <p>Email: {currentUser.email}</p>
-            <p>Username: {currentUser.username}</p>
+        <div className='alert alert-light'>
+            Please log in first
         </div>
+
+        // <div>
+        //     <h2>User Profile</h2>
+        //     <p>Name: {currentUser.name}</p>
+        //     <p>Email: {currentUser.email}</p>
+        //     {/*<p>Username: {currentUser.username}</p>*/}
+        // </div>
 
     )
 }
